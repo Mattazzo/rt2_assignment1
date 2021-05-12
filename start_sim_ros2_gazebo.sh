7f6ab3fccc3ed4abad47d1983165ec4a30f5f542
Mattazzo <mattazzo99@gmail.com>
2021-05-12 19:48:48 +0000

    fixed command in scripts

--- start_sim_ros2_gazebo.sh
+++ start_sim_ros2_gazebo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gnome-terminal --tab --title="ros1_gazebo" -- bash -c "source ros.sh; roslaunch rt2_assignment1 sim_ros2_gazebo.launch"
+gnome-terminal --tab --title="ros1_gazebo" -- bash -c "source ros.sh; cd Desktop/my_ros; roslaunch rt2_assignment1 sim_ros2_gazebo.launch"
 gnome-terminal --tab --title="bridge" -- bash -c "sleep 3; source ros12.sh; cd Desktop/my_ros2; ros2 run ros1_bridge dynamic_bridge "
 gnome-terminal --tab --title="ros2" -- bash -c "sleep 3; source ros2.sh; cd Desktop/my_ros2; ros2 launch rt2_assignment1 launch.py" 
